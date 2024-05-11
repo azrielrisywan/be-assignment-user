81617ccea07e9c5f9994a41539de937a6e1f72c5
Azriel Attaufiqi <azrielrisywan@gmail.com>
2024-05-11 21:52:16 +0700

    chore: Update Dockerfile and main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
     r := router.SetupRouter()
 
     // Start the Gin server
-    err := r.Run("localhost:8888")
+    err := r.Run("0.0.0.0:8888")
     if err != nil {
         fmt.Println("Failed to start Gin server:", err)
     }
